b06e2157f58c0e0c48c1ac245dd945f0727ee34a
SergeiVlasov1 <vlasovsergei1988@gmail.com>
2022-06-04 21:35:08 +0400

    Check Cl

--- src/test/java/ru/netology/post/PostmanEchoTest.java
+++ src/test/java/ru/netology/post/PostmanEchoTest.java
@@ -19,6 +19,6 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some value"));
     }
 }
