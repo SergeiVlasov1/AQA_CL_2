749229bc4e6835593037820283268ea286c87a18
SergeiVlasov1 <vlasovsergei1988@gmail.com>
2022-06-04 21:22:41 +0400

    Initial commit

---
+++ src/test/java/ru/netology/post/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldPostToPostmanEcho() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
